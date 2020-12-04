d4eb823d2dc419344945ca57523367b2613a350b
fengchang <fengchang_fight@163.com>
2020-12-04 22:29:13 +0800

    Update fund_filter_high_20p.py

--- python2/fund_filter_high_20p.py
+++ python2/fund_filter_high_20p.py
@@ -11,16 +11,20 @@ todayStr = formatDate(date.today())
 
 input = "/root/funddata/funddata_"+todayStr+".csv"
 output = "/root/funddata/funddata_"+todayStr+"_recommend_20.csv"
-
+blacklistfile = "/root/funddata/blacklist.txt"
+blacklist =  set(open(blacklistfile).read().split())
 def zhangfu(num1, num2):
     return (num2-num1)/num1
 
+
 if __name__ == "__main__":
     iter_csv = pd.read_csv(input, encoding='gbk', iterator=True, chunksize=1000)
+
     df = pd.concat([chunk[(chunk[u'当前净值'] > chunk[u'一年前净值'])
     & (chunk[u'当前净值'] > chunk[u'三年前净值'])
     & (zhangfu(chunk[u'一年前净值'],chunk[u'当前净值'])>zhangfu(chunk[u'三年前净值'],chunk[u'当前净值']))
     & (zhangfu(chunk[u'一年前净值'],chunk[u'当前净值'])>0.2)] for chunk in iter_csv])
 
-    print(df)
-    df.to_csv(output, encoding='utf-8-sig', index=False)
+    boolean_series = ~df[u'代码'].isin(blacklist)
+    filtered_df = df[boolean_series]
+    filtered_df.to_csv(output, encoding='utf-8-sig', index=False)
